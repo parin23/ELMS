c9777b8b8e089f23bb00478a3554e5b0b30152a3
Parin Jasoliya <parinjasoliya@gmail.com>
2021-11-25 11:38:49 +0530

    Added Function for generic table implementation

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from mysite.models import stModel
 
+def lister(obj):
+    obj1 = []
+    for i,s in enumerate(obj):
+        temp = s.__dict__
+        temp =  list( temp.values() )
+        temp = temp[1::]
+        obj1.append(temp)
+
+    return obj1
+
 def showSt (request):
     showAll = stModel.objects.all()
-    return render(request,'index.html',{"data":showAll})
+    colName = ["Student Id","Student Name"]
+    final = lister(showAll)
+    return render(request,'index.html',{"data":final,"cols":colName})
